8532b467bb2a2c9d6a2affeae575e5df7a96528c
sdidd <sdidd@LAPTOP-H2R1HR3O>
2022-05-28 12:18:57 +0530

    added mongo not working

--- gradleproject1/app/src/main/java/gradleproject1/databaseController.java
+++ gradleproject1/app/src/main/java/gradleproject1/databaseController.java
@@ -11,8 +11,18 @@ import java.sql.*;
  */
  
 public class databaseController{
- 
-public static void main(String[] args) throws SQLException 
-{
-
+    public static void main(String[] args) {
+        // Replace the uri string with your MongoDB deployment's connection string
+        String uri = "mongodb://user:pass@sample.host:27017/?maxPoolSize=20&w=majority";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("admin");
+            try {
+                Bson command = new BsonDocument("ping", new BsonInt64(1));
+                Document commandResult = database.runCommand(command);
+                System.out.println("Connected successfully to server.");
+            } catch (MongoException me) {
+                System.err.println("An error occurred while attempting to run a command: " + me);
+            }
+        }
+    }
 }
